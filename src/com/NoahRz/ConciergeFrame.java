2c5110963bc55f0f91d768dc8f2b0bc1124acfc8
Noah Razafindrabe <noah.razafindrabe@outlook.com>
2020-05-14 14:36:21 +0200

    fixing bug

--- src/com/NoahRz/ConciergeFrame.java
+++ src/com/NoahRz/ConciergeFrame.java
@@ -13,6 +13,7 @@ public class ConciergeFrame extends JFrame implements ActionListener {
      * from bavard asking to listen other bavard
      * */
     private Concierge concierge;
+    private JPanel requestPanel;
 
     public ConciergeFrame(Concierge concierge){
         this.concierge=concierge;
@@ -52,10 +53,11 @@ public class ConciergeFrame extends JFrame implements ActionListener {
         this.setJMenuBar(menubar);
 
         /*-- Panel where we display request the concierge receives --*/
-        JPanel requestPanel = new JPanel();
+        requestPanel = new JPanel();
         requestPanel.setBackground(Color.YELLOW);
         requestPanel.setLayout(new BoxLayout(requestPanel, BoxLayout.Y_AXIS)); //panel where who gathers all the request the concierge receives
 
+        //this.refreshRequestList();
         if(concierge.getRequestEventArrayList().size() == 0){ // if there is no request
             JLabel noRequestLabel = new JLabel("There is no request.");
             requestPanel.add(noRequestLabel);
@@ -103,12 +105,14 @@ public class ConciergeFrame extends JFrame implements ActionListener {
                         public void actionPerformed(ActionEvent e) { //we approve the request
                             if (re.getRequest().equals("add")) {
                                 concierge.getBavardListenersOfBavard(re.getBavardSubject()).add(bavardRequester);
+                                concierge.getRequestEventArrayList().remove(re); //we remove the request we have just handled
                                 requestPanel.remove(oneRequestPanel);
                                 requestPanel.revalidate();
                                 requestPanel.repaint();
                             }
                             if (re.getRequest().equals("remove")) {
                                 concierge.getBavardListenersOfBavard(re.getBavardSubject()).remove(bavardRequester);
+                                concierge.getRequestEventArrayList().remove(re); //we remove the request we have just handled
                                 requestPanel.remove(oneRequestPanel);
                                 requestPanel.revalidate();
                                 requestPanel.repaint();
@@ -119,6 +123,7 @@ public class ConciergeFrame extends JFrame implements ActionListener {
                         @Override
                         public void actionPerformed(ActionEvent e) {
                             requestPanel.remove(oneRequestPanel);
+                            concierge.getRequestEventArrayList().remove(re); //we remove the request we have just handled
                             requestPanel.revalidate();
                             requestPanel.repaint();
                         }
@@ -146,6 +151,87 @@ public class ConciergeFrame extends JFrame implements ActionListener {
         if(e.getActionCommand().equals("Sign out")){ //if we clicked "sign out" in the menu bar
             this.concierge.setConnected(false);
             this.dispose();
+        }
+    }
+
+    public void refreshRequestList(){
+        /**
+         * refresh the panel requestPanel which contains list of request
+         * */
+
+        requestPanel.removeAll();
+        requestPanel.revalidate();
+        requestPanel.repaint();
+
+        if(concierge.getRequestEventArrayList().size() == 0){ // if there is no request
+            JLabel noRequestLabel = new JLabel("There is no request.");
+            requestPanel.add(noRequestLabel);
+        }
+        else{
+            for (RequestEvent re : concierge.getRequestEventArrayList()) {
+                Bavard bavardRequester = (Bavard) re.getSource();
+                ArrayList<Bavard> bavardListeners = this.concierge.getBavardListenersOfBavard(re.getBavardSubject()); // listeners of the bavard subject of the request
+                if ((re.getRequest().equals("add") && bavardListeners.contains(bavardRequester)) || (re.getRequest().equals("remove") && !bavardListeners.contains(bavardRequester))) {
+                /*
+                we check that the request is still make sense : ex if we want to add a new bavard to the listeners of the bavardSubject, we have to make sure that the bavard is
+                not already among bavardSubject's listeners. Same thing if we want to remove a bavard from the listeners of the bavardSubject, we have to make sure that the bavard
+                is  among the bavardSubject's listeners.
+                this case can occur, if the concierge receives a new request but instead of directly manage it, the concierge goes to the AdjustBavardListenerFrame and do it manually
+                */
+                    System.out.println(re.getBavardSubject().getLogin());
+                    concierge.getRequestEventArrayList().remove(re);
+                }
+            }
+
+            for (RequestEvent re : concierge.getRequestEventArrayList()) { // we display the requests
+                Bavard bavardRequester = (Bavard) re.getSource();
+                JPanel oneRequestPanel = new JPanel(); //panel which gathers the request label, an approve button and an dismiss button
+                oneRequestPanel.setLayout(new BorderLayout());
+
+                String preposition;
+                if (re.getRequest().equals("add")) {
+                    preposition = " to his listening";
+                } else { //it's "remove"
+                    preposition = " from his listening";
+                }
+                JLabel requestLabel = new JLabel(bavardRequester.getLogin() + " wants to " + re.getRequest() + " " + re.getBavardSubject().getLogin() + preposition);
+
+                    /* we add an approve button and a dismiss button next to the request so that the concierge can directly answer the request
+                    without going to the AdjustBavardListenerFrame where the concierge will have to do it manually */
+                JButton approveButton = new JButton("Approve");
+                JButton dismissButton = new JButton("Dismiss");
+
+                oneRequestPanel.add(requestLabel, BorderLayout.WEST);
+                oneRequestPanel.add(approveButton, BorderLayout.CENTER);
+                oneRequestPanel.add(dismissButton, BorderLayout.EAST);
+
+                approveButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) { //we approve the request
+                        if (re.getRequest().equals("add")) {
+                            concierge.getBavardListenersOfBavard(re.getBavardSubject()).add(bavardRequester);
+                            requestPanel.remove(oneRequestPanel);
+                            requestPanel.revalidate();
+                            requestPanel.repaint();
+                        }
+                        if (re.getRequest().equals("remove")) {
+                            concierge.getBavardListenersOfBavard(re.getBavardSubject()).remove(bavardRequester);
+                            requestPanel.remove(oneRequestPanel);
+                            requestPanel.revalidate();
+                            requestPanel.repaint();
+                        }
+                    }
+                });
+                dismissButton.addActionListener(new ActionListener() { // we dismiss the request
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        requestPanel.remove(oneRequestPanel);
+                        requestPanel.revalidate();
+                        requestPanel.repaint();
+                    }
+                });
+                requestPanel.add(oneRequestPanel);
+            }
         }
     }
 }
